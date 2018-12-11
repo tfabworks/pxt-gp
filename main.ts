18af09f8b6dc6f491343a14f1d1cf6d016c9a3f2
plainbanana <s1820650@s.tsukuba.ac.jp>
2018-12-11 15:56:12 +0900

    add func (get analog and brightness determination)

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+enum BrightnessEvent {
+    //% block="暗い"
+    IsDark = 1,
+    //% block="明るい"
+    IsBrighter = 2,
+}
+
 //% weight=70 icon="\uf0e7" color=#d2691e block="電気の利用"
 namespace gp {
     //% blockId=human_detection block="人が動いた %v"
@@ -22,6 +29,28 @@ namespace gp {
             return true;
         } else {
             return false;
+        }
+    }
+    //% blockId=get_analog_value block="%p からアナログ値を取得"
+    export function getAnalogValue(p: AnalogPin): number {
+        let val: number;
+        for (; ;) {
+            if ((val = pins.analogReadPin(p)) != 255)
+                break;
+        }
+        return val;
+    }
+    //% blockId=brightness_determination block="%v より %flag"
+    //% v.min=0 v.max=255
+    export function brightnessDetermination(v: number, flag: BrightnessEvent): boolean {
+        let res: boolean = true;
+        if (flag == 2) {
+            res = !res;
+        }
+        if (input.lightLevel() < v) {
+            return res;
+        } else {
+            return !res;
         }
     }
 }
