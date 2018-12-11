7b03e34754e98b0be93072fb87c87021ffdf7cb7
plainbanana <s1820650@s.tsukuba.ac.jp>
2018-12-11 16:01:03 +0900

    minor changes

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ enum BrightnessEvent {
 
 //% weight=70 icon="\uf0e7" color=#d2691e block="電気の利用"
 namespace gp {
-    //% blockId=human_detection block="人が動いた %v"
+    //% blockId=human_detection block="人が動いた"
     export function humanDetection(): boolean {
         if (pins.digitalReadPin(DigitalPin.P2) == 1) {
             return true;
@@ -15,15 +15,15 @@ namespace gp {
             return false;
         }
     }
-    //% blockId=turn_on block="スイッチON %v"
+    //% blockId=turn_on block="スイッチON"
     export function turnON(): void {
         pins.digitalWritePin(DigitalPin.P1, 1)
     }
-    //% blockId=turn_off block="スイッチOFF %v"
+    //% blockId=turn_off block="スイッチOFF"
     export function turnOFF(): void {
         pins.digitalWritePin(DigitalPin.P1, 0)
     }
-    //% blockId=is_dark block="暗い %v"
+    //% blockId=is_dark block="暗い"
     export function isDark(): boolean {
         if (input.lightLevel() < 30) {
             return true;
